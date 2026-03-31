f69a9f9ece40dd3ecd40b70be13bb1b8bf77e6a1
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-03-31 21:38:28 +0700

    Fix formatting issues in tugaslooping.cpp

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -48,13 +48,3 @@ bool isFibonacci(int n)
 
 void tampilkanHasil(bool hasil, string jenis)
 
-{
-    if (hasil)
-    {
-        cout << "hasil: " << angka << " adalah bagian dari " << jenis << "." << endl;
-    } else
-    {
-        cout << "hasil: " << angka << " bukan bagian dari " << jenis << "." << endl;
-    }
-}
-
