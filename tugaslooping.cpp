c88500f45f95b567a229d38e9dab923585a0877d
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-03-31 21:28:34 +0700

    Add menu display function to tugaslooping.cpp

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -4,3 +4,12 @@ using namespace std;
 int angka;
 int pilihan;
 
+void tampilkanMenu() 
+{
+    cout << "\n===== MENU UTAMA =====" << endl;
+    cout << "1. cek bilangan prima" << endl;
+    cout << "2. cek bilangan fibonacci" << endl;
+    cout << "0. keluar" << endl;
+    cout << "pilih menu" << endl;
+    cin >> pilihan;
+}
